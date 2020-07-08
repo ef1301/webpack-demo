a32688e04d2400a873997c68068691fd50ecf297
ef1301 <fangemily60.com>
2020-07-08 12:54:20 -0400

    styles

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,9 @@ module.exports = {
           'file-loader',
         ],
       },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/icon-woff" }
     ]
   },
   devtool: 'inline-source-map',
